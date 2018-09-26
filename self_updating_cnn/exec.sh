389081b4e23e64810ea447c0da02a82366206d15
Bojana Mandić <mandicbojana003@gmail.com>
2018-09-26 16:29:24 +0200

    Delete exec.sh

--- self_updating_cnn/exec.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-gimp -idf --batch-interpreter=python-fu-eval -b 'import sys; sys.path=["."]+sys.path;import gimp_script;gimp_script.create_cat_image("face_image.png", "coordinates.txt")'
-
